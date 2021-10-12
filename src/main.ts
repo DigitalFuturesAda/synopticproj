aafbd74d5186b8465c9029e83f75bbd47de30594
zv <zv@zv.wtf>
2021-10-12 12:38:12 +0100

    Added Chakra UI framework

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './ui/root/AppRoot.vue'
 import router from './data/router'
 import store from './data/store'
+import Chakra from '@chakra-ui/vue'
+
+Vue.use(Chakra)
 
 Vue.config.productionTip = false
 
